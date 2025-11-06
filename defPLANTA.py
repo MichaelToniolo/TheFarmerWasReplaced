64b42dbd354233f30e48c6f74b74bb2438a590eb
Michael (a.k.a) Mz3ro <37878223+MichaelToniolo@users.noreply.github.com>
2025-11-06 15:45:44 -0300

    Adjust water thresholds for plant functions

--- defPLANTA.py
+++ defPLANTA.py
@@ -2,26 +2,30 @@
 def Giras():
 	till()
 	plant(Entities.Sunflower)
-	if get_water() <= 1 and num_items(Items.Water) > 20 and get_entity_type() == Entities.Sunflower:
+	if get_water() <= 1 and num_items(Items.Water) > 200 and get_entity_type() == Entities.Sunflower:
 		use_item(Items.Water)
 	if can_harvest():
 		harvest()
 
-#PLNTAABRAS
+#PLNATAABRAS
 def Abras():
-	till()
-	plant(Entities.Pumpkin)
-	if get_water() <= 1 and num_items(Items.Water) > 20 and get_entity_type() == Entities.Pumpkin:
-		use_item(Items.Water)
-
-	if can_harvest():
+	if get_ground_type() != Grounds.Soil:
+			till()
+			plant(Entities.Pumpkin)
+			if get_water() <= 1 and num_items(Items.Water) > 200 and get_entity_type() == Entities.Pumpkin:
+				use_item(Items.Water)
+	elif can_harvest() == False:
+		plant(Entities.Pumpkin)
+	else:
 		harvest()
+			
+		
 #PLANTAZENAS
 def Zenas():
 	if can_harvest():
 		harvest()
 	till()
 	plant(Entities.Carrot)
-	if get_water() <= 1 and num_items(Items.Water) > 240 and get_entity_type() == Entities.Carrot:
+	if get_water() <= 1 and num_items(Items.Water) > 200 and get_entity_type() == Entities.Carrot:
 		use_item(Items.Water)
 	
