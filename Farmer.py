9f49fb49ea4ad6402ec4de24a5278be9c85dafac
Michael (a.k.a) Mz3ro <37878223+MichaelToniolo@users.noreply.github.com>
2025-11-06 15:45:24 -0300

    REFATORADA

--- Farmer.py
+++ Farmer.py
@@ -1,26 +1,24 @@
 pos = 0
-import PLANTA, DEFS_TEST
+import PLANTA
 while True:
-	for i in range(6):
-#POSX 1 E 3
-		if get_pos_x() == 1:
-			PLANTA.Zenas()
-#POSX 0 E 2
+	for i in range(8):
+
+#POSX 0 ARVRE + GIRAS
 		if get_pos_x() == 0:
 			if pos % 2 == 0:
 				plant(Entities.Tree)
 			else:
-				plant(Entities.Bush)
+				PLANTA.Giras()
 		if can_harvest():
 				harvest()
-#POSX 4 E 5
-		if (get_pos_x() == 4 or get_pos_x() == 5) and (get_pos_y() == 2 or get_pos_y() == 3 or get_pos_y() == 4 or get_pos_y() == 5):
+#POSX 1 ZENAS
+		if get_pos_x() == 1:
 			PLANTA.Giras()
+#POSX 2 E 5
+		if (get_pos_x() == 2 or get_pos_x() == 3):
+			PLANTA.Zenas()
 #POSX 4 POSY 3
-		if (get_pos_x() == 4 or get_pos_x() == 5) and (get_pos_y() == 0 or get_pos_y() == 1):
-
-			PLANTA.Abras()
-		if get_pos_x() == 2 or get_pos_x() == 3:
+		if get_pos_x() == 4 or get_pos_x() == 5 or get_pos_x() == 6 or get_pos_x() == 7:
 			PLANTA.Abras()
 		move(North)
 		pos += 1
